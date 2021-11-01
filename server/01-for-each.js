7a53223293496946e6726570510010808b6c19ac
lfer9980 <luis_fernandez99@hotmail.com>
2021-11-01 14:07:46 -0700

    clase 1: forEach

---
+++ server/01-for-each.js
@@ -0,0 +1,9 @@
+const letters = ['a','b','c']
+
+for (let i = 0; i < letters.length; i++) {
+	const element = letters[i];
+	console.log('for', element)
+}
+
+letters.forEach(item => console.log('foreach',item))
+

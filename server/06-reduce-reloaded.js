4da6a4a2ec1d20a762575b2a4f7852cf67899a1d
lfer9980 <luis_fernandez99@hotmail.com>
2021-11-04 21:09:42 -0700

    clase 8: reduce reloaded

---
+++ server/06-reduce-reloaded.js
@@ -0,0 +1,61 @@
+const items = [1,3,2,3];
+
+//output
+/* {
+	1: 1,
+	3: 2,
+	2: 1,
+} */
+
+const rta = items.reduce((obj, item) => {
+	if(!obj[item]) {
+		obj[item] = 1
+	} else {
+		obj[item] = obj[item] + 1
+	}
+	return obj
+}, {})
+
+console.log(rta)
+
+
+//utilizando reduce con objetos
+const data = [
+	{
+		name: "Nicolas",
+		level: "low",
+	},
+	{
+		name: "Andrea",
+		level: "medium",
+	},
+	{
+		name: "Zulema",
+		level: "hight",
+	},
+	{
+		name: "Santiago",
+		level: "low",
+	},
+	{
+		name: "Valentina",
+		level: "medium",
+	},
+	{
+		name: "Lucia",
+		level: "hight",
+	},
+];
+
+const rta1 = data
+.map(item => item.level)
+.reduce((obj,item) => {
+	if(!obj[item]) {
+		obj[item] = 1
+	} else {
+		obj[item] += 1
+	}
+	return obj
+}, {})
+
+console.log(rta1)

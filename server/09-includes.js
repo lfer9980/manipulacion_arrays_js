434029a656f30586254e639e6723ff574ebe9af3
lfer9980 <luis_fernandez99@hotmail.com>
2021-11-06 12:46:08 -0700

    clase 12: includes

---
+++ server/09-includes.js
@@ -0,0 +1,17 @@
+const pets = ['cat', 'dog', 'bat']
+
+let includeInArray = false
+
+for (let index = 0; index < pets.length; index++) {
+	const element = pets[index];
+	if (element === 'dog') {
+		includeInArray = true
+		break
+	}
+}
+
+console.log('for', includeInArray)
+
+//utilizando includes
+const rta = pets.includes('dog')
+console.log('include', rta)
